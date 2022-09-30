89f220348fab63edc6b572c6529df3c16a3cd746
nenebeji <105363883+nenebeji@users.noreply.github.com>
2022-09-30 13:00:00 +0100

    added endpoint for product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
-
+router.use('/products');
 // get all products
 router.get('/', (req, res) => {
   // find all products
