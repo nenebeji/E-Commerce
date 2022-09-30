60f3653de61fed8539efe330c0a4945d4f994f77
nenebeji <105363883+nenebeji@users.noreply.github.com>
2022-09-30 14:45:41 +0100

    synced sequelize models to database

--- server.js
+++ server.js
@@ -8,9 +8,10 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// turn on routes
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+})
