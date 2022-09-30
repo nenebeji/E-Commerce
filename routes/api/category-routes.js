751ab54953f23e7c94536790f4bb92cba43ebda1
nenebeji <105363883+nenebeji@users.noreply.github.com>
2022-09-30 13:01:37 +0100

    added endpoint for categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
+router.use('/categories');
 
 router.get('/', (req, res) => {
   // find all categories
