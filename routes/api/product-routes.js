1474b3c074634e7e2c5251d07136085b0e2a3201
nenebeji <105363883+nenebeji@users.noreply.github.com>
2022-09-30 13:34:29 +0100

    added find product by ID

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -19,9 +19,21 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  try {
+    const productsData = await Product.findByPk(req.params.id, {
+      //include its associated Category and Tag data
+      include: [{model: Category}, {model: Tag, through: ProductTag}]
+    });
+    if (!productsData) {
+      res.status(404).json({ message: 'No Product found with this id!' });
+      return;
+    }
+    res.status(200).json(productsData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
